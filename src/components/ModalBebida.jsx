d45ebdaca3c6c8ddd2c743052cf15144307d3350
zassory <nicolas.programador@gmail.com>
2022-12-20 14:58:36 -0300

    muestra imagen e informacion, se muestran ingredientes

--- src/components/ModalBebida.jsx
+++ src/components/ModalBebida.jsx
@@ -3,17 +3,44 @@ import { useBebidas } from '../hooks';
 
 export const ModalBebida = () => {
 
-  const { modal , handleModalClick } = useBebidas();
+  const { modal , handleModalClick , receta } = useBebidas();
 
-  
+  console.log(receta);
+
+  const mostrarIngredientes = () => {
+    let ingredientes = [];
+
+    for(let i = 1;i < 16;i++){
+      if(receta[`strIngredient${i}`]){
+        ingredientes.push(
+          <li>
+            {receta[`strIngredient${i}`]} {receta[`strMeasure${i}`]}
+          </li>
+        )
+      }
+    }
+    return ingredientes;
+  }
 
   return (
     <Modal show={modal} onHide={handleModalClick}>
+      <Image 
+        src={receta.strDrinkThumb}
+        alt={`Imagen recera ${receta.strDrink}`}
+      />
+      
+      <Modal.Header>
+        <Modal.Title>{receta.strDrink}</Modal.Title>
+      </Modal.Header>
 
-
-        <Modal.Body>
-            Cuerpo de Modal
-        </Modal.Body>
+      <Modal.Body>
+          <div className='p-3'>
+            <h2>Instrucciones</h2>
+            {receta.strInstructions}
+            <h2>Ingredientes y Cantidades</h2>
+            {mostrarIngredientes()}
+          </div>
+      </Modal.Body>
     </Modal>
   )
 }
